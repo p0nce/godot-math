ea2d7abd73b2c99297c172f5151c90c74c70e6f9
Guillaume Piolat <contact@auburnsounds.com>
2025-12-20 13:27:00 +0100

    Transform2D tests

--- tests/transform2d.d
+++ tests/transform2d.d
@@ -23,3 +23,17 @@ unittest
     assert(A.is_equal_approx(Vector2(0, -4)));
 
 }
+
+@("Transform2D interpolate_with")
+unittest
+{
+    Transform2D T1 = Transform2D(0, Vector2(0, 0));
+    Transform2D T2 = Transform2D(GM_PI / 2.0f, Vector2(10, 10));
+    
+    Transform2D interpolated = T1.interpolate_with(T2, 0.5f);
+    
+    // Interpolated rotation should be halfway
+    assert(gm_is_equal_approx(interpolated.get_rotation(), cast(float)(GM_PI / 4.0f)));
+    // Interpolated origin should be halfway
+    assert(interpolated.get_origin().is_equal_approx(Vector2(5, 5)));
+}
