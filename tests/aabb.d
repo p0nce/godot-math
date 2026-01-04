c22949c280f37866106fb67272cc2417c4fde2f6
Guillaume Piolat <contact@auburnsounds.com>
2026-01-04 18:38:16 +0100

    Okay

---
+++ tests/aabb.d
@@ -0,0 +1,39 @@
+module aabb;
+
+import godotmath;
+
+pure nothrow @nogc @safe:
+
+@("AABB abs")
+unittest
+{
+    AABB box = AABB(Vector3(5, 0, 5), Vector3(-20, -10, -5));
+    AABB absolute = box.abs();
+    assert(absolute.position == Vector3(-15.0, -10.0, 0.0));
+    assert(absolute.size == Vector3(20.0, 10.0, 5.0));
+}
+  
+@("AABB encloses")
+unittest
+{
+    AABB a = AABB(Vector3(0, 0, 0), Vector3(4, 4, 4));
+    AABB b = AABB(Vector3(1, 1, 1), Vector3(3, 3, 3));
+    AABB c = AABB(Vector3(2, 2, 2), Vector3(8, 8, 8));
+    assert(a.encloses(a)); 
+    assert(a.encloses(b)); 
+    assert(!a.encloses(c));
+}
+
+@("AABB expand")
+unittest
+{
+    AABB box = AABB(Vector3(0, 0, 0), Vector3(5, 2, 5));
+
+    box = box.expand(Vector3(10, 0, 0));
+    assert(box.position == Vector3(0.0, 0.0, 0.0));
+    assert(box.size  == Vector3(10.0, 2.0, 5.0));
+
+    box = box.expand(Vector3(-5, 0, 5));
+    assert(box.position == Vector3(-5.0, 0.0, 0.0));
+    assert(box.size  == Vector3(15.0, 2.0, 5.0));
+}
